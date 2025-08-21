32f72a13c13707d93e454fb22df95aec56925711
maher24m <123480723+maher24m@users.noreply.github.com>
2025-08-21 10:36:46 +0800

    feat: auth module for mobile

---
+++ prisma/migrations/20250820181009_finalize_auth_schema/migration.sql
@@ -0,0 +1,80 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `access_token` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `expires_at` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `id_token` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `refresh_token` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `session_state` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `token_type` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `expires` on the `Session` table. All the data in the column will be lost.
+  - You are about to drop the column `sessionToken` on the `Session` table. All the data in the column will be lost.
+  - You are about to drop the column `displayUsername` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `username` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `VerificationToken` table. If the table is not empty, all the data it contains will be lost.
+  - A unique constraint covering the columns `[token]` on the table `Session` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `expiresAt` to the `Session` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `token` to the `Session` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Session` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `email` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropIndex
+DROP INDEX "public"."Session_sessionToken_key";
+
+-- DropIndex
+DROP INDEX "public"."User_displayUsername_key";
+
+-- DropIndex
+DROP INDEX "public"."User_username_key";
+
+-- AlterTable
+ALTER TABLE "public"."Account" DROP COLUMN "access_token",
+DROP COLUMN "expires_at",
+DROP COLUMN "id_token",
+DROP COLUMN "refresh_token",
+DROP COLUMN "session_state",
+DROP COLUMN "token_type",
+ADD COLUMN     "accessToken" TEXT,
+ADD COLUMN     "accessTokenExpiresAt" TIMESTAMP(3),
+ADD COLUMN     "idToken" TEXT,
+ADD COLUMN     "refreshToken" TEXT,
+ADD COLUMN     "refreshTokenExpiresAt" TIMESTAMP(3);
+
+-- AlterTable
+ALTER TABLE "public"."Session" DROP COLUMN "expires",
+DROP COLUMN "sessionToken",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "expiresAt" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "ipAddress" TEXT,
+ADD COLUMN     "token" TEXT NOT NULL,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "userAgent" TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."User" DROP COLUMN "displayUsername",
+DROP COLUMN "username",
+ALTER COLUMN "name" SET NOT NULL,
+ALTER COLUMN "email" SET NOT NULL;
+
+-- DropTable
+DROP TABLE "public"."VerificationToken";
+
+-- CreateTable
+CREATE TABLE "public"."Verification" (
+    "id" TEXT NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Verification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Verification_value_key" ON "public"."Verification"("value");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_token_key" ON "public"."Session"("token");
