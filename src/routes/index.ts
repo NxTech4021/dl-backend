f5f1a7a68e4328a0a4ac6d8e4ddcd5e43e0da0ca
Zawad Wasik Ahmed <69716177+ZawadAhmed@users.noreply.github.com>
2025-09-29 18:04:41 +0600

    Routes naming

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,33 @@
 import express from "express";
-
-import { auth } from "../lib/auth";
+import adminRouter from "./adminRoutes";
+import playerRouter from "./playerRoutes";
+import seasonRouter from "./seasonRoutes";
 
 const router = express.Router();
-
-import adminrouter from "./adminRoutes";
-import playerRouter from "./playerRoutes";
 
 router.get("/health", (req, res) => {
   res.json({ status: "OK", message: "Server is running" });
 });
 
-router.use("/api/admin", adminrouter);
+router.use("/api/admin", adminRouter);
 router.use("/api/player", playerRouter);
+
+// Divisions, Leagues,  Matches & Seasons
+
+// router.use("/api/league", playerRouter);
+// router.use("/api/division", playerRouter);
+// router.use("/api/match", playerRouter);
+router.use("/api/season", seasonRouter);
+
+// LeaderBoard 
+
+// Chat 
+
+
+// Settings 
+
+// Notification?
+
 
 // router.post("/api/")
 
