09d953fffadd3ad5a9e23e5b0be5c91ebc1932dd
nekkaida <kennethriadinugroho@gmail.com>
2025-10-30 10:54:15 +0800

    Update registration service

--- src/services/registrationService.ts
+++ src/services/registrationService.ts
@@ -1,6 +1,6 @@
+import { prisma } from "../lib/prisma";
 import { PrismaClient, RegistrationType } from '@prisma/client';
 
-const prisma = new PrismaClient();
 
 interface PlayerRegistrationData {
   playerId: string;
