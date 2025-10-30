8f828b423093ca79f508824c685fdd6088b65ea7
nekkaida <kennethriadinugroho@gmail.com>
2025-10-30 10:48:38 +0800

    Update league type service

--- src/services/leagueTypeService.ts
+++ src/services/leagueTypeService.ts
@@ -1,6 +1,6 @@
+import { prisma } from "../lib/prisma";
 import { PrismaClient, LeagueTypeType, Gender } from '@prisma/client';
 
-const prisma = new PrismaClient();
 
 interface LeagueTypeCreationData {
   name: string;
