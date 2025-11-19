5c90a818e3b0fab32bf4b3148bfeb8ba9b7c6589
nekkaida <kennethriadinugroho@gmail.com>
2025-11-19 15:53:19 +0800

    feat(ratings): add rating services barrel export

---
+++ src/services/rating/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Rating Services - Barrel Export
+ */
+
+export * from './ratingCalculationService';
+export * from './standingsCalculationService';
+export * from './playerRatingService';
+export * from './adminRatingService';
+
+export { getRatingCalculationService } from './ratingCalculationService';
+export { getStandingsCalculationService } from './standingsCalculationService';
+export { getPlayerRatingService } from './playerRatingService';
+export { getAdminRatingService } from './adminRatingService';
